5cbaae6775f7d533504677224b8dbdd182b2e081
Seth Walsh <seth.walsh@utah.edu>
2014-12-18 14:55:24 -0700

    First commit

---
+++ PrinterManager/PrinterManager/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace PrinterManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addPrinterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removePrinterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewInstalledPrintersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(801, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addPrinterToolStripMenuItem,
+            this.removePrinterToolStripMenuItem,
+            this.viewInstalledPrintersToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // addPrinterToolStripMenuItem
+            // 
+            this.addPrinterToolStripMenuItem.Name = "addPrinterToolStripMenuItem";
+            this.addPrinterToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.addPrinterToolStripMenuItem.Text = "Add Printer";
+            // 
+            // removePrinterToolStripMenuItem
+            // 
+            this.removePrinterToolStripMenuItem.Name = "removePrinterToolStripMenuItem";
+            this.removePrinterToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.removePrinterToolStripMenuItem.Text = "Remove Printer";
+            // 
+            // viewInstalledPrintersToolStripMenuItem
+            // 
+            this.viewInstalledPrintersToolStripMenuItem.Name = "viewInstalledPrintersToolStripMenuItem";
+            this.viewInstalledPrintersToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.viewInstalledPrintersToolStripMenuItem.Text = "View Installed Printers";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Printer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Remove Printer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(153, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(636, 197);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(153, 241);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 424);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Printer Manger 1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addPrinterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removePrinterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewInstalledPrintersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ PrinterManager/PrinterManager/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.Management;
+using System.Net;
+using System.IO;
+
+namespace PrinterManager
+{
+    public partial class Form1 : Form
+    {
+        List<string> _printers = new List<string>();
+        List<string> _currentPrinters = new List<string>();
+
+        private string current_server = "", location_property_name = "", working_directory = "", download_path = "", key_value_filename = "", printer_list_filename = "";
+        private string[] servers = new string[] { };
+
+        public Form1()
+        {
+            // Read config file
+            ReadConfigFile();
+            
+            // Get all available printers and add them into the available list
+            RetrievePrinterList();
+
+            this.listView1 = new ListView();
+            GetCurrentPrinterList();
+
+            InitializeComponent();
+
+            listBox1.DataSource = _printers;            
+        }
+
+        public void GetCurrentPrinterList()
+        {            
+            // Search all installed printers and retrieve the comment property string that will identify which printers this service previously installed on the system
+            System.Management.ManagementObjectSearcher searcher = new System.Management.ManagementObjectSearcher("SELECT * FROM Win32_Printer");
+            foreach (System.Management.ManagementObject printer in searcher.Get())
+            {
+                string location = "";
+                if (printer["Location"] != null)
+                    location = printer["Location"].ToString();
+                if (location.Contains(location_property_name) && printer["Location"] != null)
+                {
+                    // This is one of our network printers
+                    listView1.Items.Add(printer["Name"].ToString());
+                }
+            }            
+        }
+
+        public void ReadConfigFile()
+        {
+            // Get print servers from config file
+            if (File.Exists(@"C:\Tools\Printing\config.txt"))
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Tools\Printing\config.txt");
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] words;
+                    if (line.Contains("[server_list]"))
+                    {
+                        string[] delim = new string[] { "= " };
+                        words = line.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+                        servers = words[1].Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+                        //Console.WriteLine("PrintingService.PrimaryServer: " + primary_server);
+                    }                    
+                    if (line.Contains("[location_property_name]"))
+                    {
+                        words = line.Split((string[])null, 3, StringSplitOptions.RemoveEmptyEntries);
+                        location_property_name = words[2];
+                    }
+                    if (line.Contains("[working_directory]"))
+                    {
+                        words = line.Split((string[])null, 3, StringSplitOptions.RemoveEmptyEntries);
+                        working_directory = words[2];
+                    }
+                    if (line.Contains("[download_path]"))
+                    {
+                        words = line.Split((string[])null, 3, StringSplitOptions.RemoveEmptyEntries);
+                        download_path = words[2];
+                    }
+                    if (line.Contains("[key_value_filename]"))
+                    {
+                        words = line.Split((string[])null, 3, StringSplitOptions.RemoveEmptyEntries);
+                        key_value_filename = words[2];
+                    }
+                    if (line.Contains("[printer_list_filename]"))
+                    {
+                        words = line.Split((string[])null, 3, StringSplitOptions.RemoveEmptyEntries);
+                        printer_list_filename = words[2];
+                    }
+                }
+                file.Close();
+            }
+        }
+
+        public void RetrievePrinterList()
+        {
+            // Download the key value printer => port file from the Server
+            WebClient client = new WebClient();
+            string url = "http://" + servers[0] + "/printing/" + key_value_filename;
+            try
+            {
+                client.DownloadFile(new Uri(url), "printer_list.txt");
+
+                if (File.Exists(@"printer_list.txt"))
+                {
+                    System.IO.StreamReader file = new System.IO.StreamReader(@"printer_list.txt");
+                    string line;
+                    
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        // Split the line and keep just the printer name
+                        string[] split = line.Split((string[])null, 3, StringSplitOptions.RemoveEmptyEntries);
+                        _printers.Add(split[0]);                        
+                    }
+                    file.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public void AddPrinter(string printer)
+        {
+            string command = @"C:\Windows\System32\spool\tools\PrintBrm.exe";
+            ProcessStartInfo startInfo = new ProcessStartInfo(command);
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(startInfo);
+            startInfo.Arguments = @"-r -f C:\Tools\Printing\" + printer + ".brm -noacl -o force";
+            var process = Process.Start(startInfo);
+            process.WaitForExit();
+            MessageBox.Show("Printer: " + printer + " has successfully been added.");
+        }
+
+        public void RemovePrinter(string printer)
+        {
+            System.Management.ManagementScope oManagementScope = new System.Management.ManagementScope(System.Management.ManagementPath.DefaultPath);
+            oManagementScope.Connect();
+            System.Management.SelectQuery query = new System.Management.SelectQuery("SELECT * FROM Win32_Printer");
+            System.Management.ManagementObjectSearcher search = new System.Management.ManagementObjectSearcher(oManagementScope, query);
+            System.Management.ManagementObjectCollection printers = search.Get();
+            foreach (System.Management.ManagementObject p in printers)
+            {
+                string pName = p["Name"].ToString().ToLower();
+                if (pName.Equals(printer.ToLower()))
+                {
+                    p.Delete();
+                    break;
+                }
+            }
+            MessageBox.Show("Printer: " + printer + " has successfully been removed.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            AddPrinter("CMS-5");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RemovePrinter("CMS-5");
+        }
+    }
+}
